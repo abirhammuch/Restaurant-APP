a1de7bc1f2934fca0bb673a467d5869702239b0e
abirham <abirhammuch526@gmail.com>
2026-07-10 10:39:43 +0300

    order eror

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -1,67 +1,75 @@
-
 import { AppContext } from "../context/AppContext";
 import Less from "./Less";
 import More from "./More";
 import Title from "./Title";
 
 import React, { useContext, useEffect, useState } from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Category = () => {
-  const {  allCategory,setFullCategory,navigate} = useContext(AppContext)
-  
-  const [more, setMore] = useState(false)
-  const [filterCategory, setFilterCategory] = useState([])
-  
-  
+  const { allCategory, setFullCategory, navigate } = useContext(AppContext);
 
+  const handleCategoryClick = (categoryName) => {
+    const path = `/menu/${categoryName.trim().toLowerCase()}`;
+    navigate(path);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
+  const [more, setMore] = useState(false);
+  const [filterCategory, setFilterCategory] = useState([]);
 
-  
-  
-  
   return (
     <div className="px-4">
       <Title text={"Explore Categories"} />
-      <div onClick={()=>setMore((prev) => !prev)}>
-        {
-          more ?  <Less text={'Less'} /> :<More   text={'View More'} /> 
-        }
-        
+      <div onClick={() => setMore((prev) => !prev)}>
+        {more ? <Less text={"Less"} /> : <More text={"View More"} />}
       </div>
-      
-      {
-        more ? <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 mt-6 gap-6 '>
-       { allCategory.map((category, index) => (
-          <div key={index} className='group cursor-pointer py-5 px-3   gap-2 rounded-lg flex flex-col justify-center items-center bg-gray-100 ' >
-            <div onClick={() => navigate(`/menu/${category.name.trim().toLowerCase()}`,
-            scrollTo(0,0))}  className="flex flex-col px-3 " >
-              <img  src={category.image} alt={category.name}className='group-hover:scale-108 transition max-w-60 rounded-2xl ' />
-              <p className="px-12 pt-5">{category.name}</p>
-            </div>
-          </div>
-        ))} : 
-        
-        
 
-        
-      </div> :<div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 mt-6 gap-6 '>
-       { allCategory.slice(0,6).map((category, index) => (
-          <div key={index} className='group cursor-pointer py-5 px-3   gap-2 rounded-lg flex flex-col justify-center items-center bg-gray-100 '>
-            <div onClick={() => navigate(`/menu/${category.name.trim().toLowerCase()}`,
-            scrollTo(0,0))}  className="flex flex-col px-3 ">
-              <img src={category.images[0]} alt={category.name}className='group-hover:scale-108 transition max-w-60 rounded-2xl'/>
-              <p className="px-12">{category.name}</p>
+      {more ? (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 mt-6 gap-6 ">
+          {allCategory.map((category, index) => (
+            <div
+              key={index}
+              className="group cursor-pointer py-5 px-3   gap-2 rounded-lg flex flex-col justify-center items-center bg-gray-100 "
+            >
+              <div
+                onClick={() => handleCategoryClick(category.name)}
+                className="flex flex-col px-3 "
+              >
+                <img
+                  src={category.image}
+                  alt={category.name}
+                  className="group-hover:scale-108 transition max-w-60 rounded-2xl "
+                />
+                <p className="px-12 pt-5">{category.name}</p>
+              </div>
             </div>
-          </div>
-        ))} : 
-        
-        
-
-        
-      </div>
-      }
-      
+          ))}{" "}
+          :
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 mt-6 gap-6 ">
+          {allCategory.slice(0, 6).map((category, index) => (
+            <div
+              key={index}
+              className="group cursor-pointer py-5 px-3   gap-2 rounded-lg flex flex-col justify-center items-center bg-gray-100 "
+            >
+              <div
+                onClick={() => handleCategoryClick(category.name)}
+                className="flex flex-col px-3 "
+              >
+                <img
+                  src={category.images[0]}
+                  alt={category.name}
+                  className="group-hover:scale-108 transition max-w-60 rounded-2xl"
+                />
+                <p className="px-12">{category.name}</p>
+              </div>
+            </div>
+          ))}{" "}
+          :
+        </div>
+      )}
     </div>
   );
 };
--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,59 +1,64 @@
-
-
-import React from 'react'
-import { useContext } from 'react'
-import { AppContext } from '../context/AppContext'
-import { FaChevronDown, FaChevronRight, FaArrowRight, FaPlus, FaExpand } from "react-icons/fa";
-
+import React from "react";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import {
+  FaChevronDown,
+  FaChevronRight,
+  FaArrowRight,
+  FaPlus,
+  FaExpand,
+} from "react-icons/fa";
 
 const Order = () => {
-
-  const {userOrder, navigate} = useContext(AppContext)
+  const { userOrder, navigate } = useContext(AppContext);
 
   return (
     <div>
-      <div className='flex gap-3 items-center justify-center mt-9 font-bold text-3xl'>
+      <div className="flex gap-3 items-center justify-center mt-9 font-bold text-3xl">
         <p>Total Orders</p>
-        <p className='text-orange-600'>3</p>
+        <p className="text-orange-600">3</p>
       </div>
-
 
       {/* order card */}
-    <div className='border m-9 rounded-2xl border-gray-200 shadow-2xl shadow-black pb-9'>
-      <div>
-          <div className='grid grid-cols-[1fr_1fr_1fr_1fr] mx-9 border-b border-gray-400 px-4 py-4 mt-9'>
-        
-        <p className='text-2xl font-bold'>OrderId</p>
-        <p  className='text-2xl font-bold'>order status</p>
-        <p className='text-2xl font-bold'>payment Status</p>
-        <p className='text-2xl font-bold'>View Details</p>
-      </div>
-      {
-        userOrder && userOrder.map((order, index) => (
-            <div key={index} className='grid grid-cols-[1fr_1fr_1fr_1fr] mx-9 border-b border-gray-400 px-4 py-4 mt-4'>
-              <p  className='text-md font-bold'>{order._id}</p>
-              <p  className={`text-md font-bold ${order.orderStatus === 'pending' ? '' : ''}`}>{order.orderStatus
-                    }</p>
-                <p  className='text-md font-bold'>{order.paymentStatus}</p>
-              <div onClick={() => navigate(`/orders/${order._id}`,
-            scrollTo(0,0))}
-              className='flex gap-2 items-center'>
-                  <p  
-                className='cursor-pointer  font-bold hover:text-orange-600'>View Detail 
-                
+      <div className="border m-9 rounded-2xl border-gray-200 shadow-2xl shadow-black pb-9">
+        <div>
+          <div className="grid grid-cols-[1fr_1fr_1fr_1fr] mx-9 border-b border-gray-400 px-4 py-4 mt-9">
+            <p className="text-2xl font-bold">OrderId</p>
+            <p className="text-2xl font-bold">order status</p>
+            <p className="text-2xl font-bold">payment Status</p>
+            <p className="text-2xl font-bold">View Details</p>
+          </div>
+          {userOrder &&
+            userOrder.map((order, index) => (
+              <div
+                key={index}
+                className="grid grid-cols-[1fr_1fr_1fr_1fr] mx-9 border-b border-gray-400 px-4 py-4 mt-4"
+              >
+                <p className="text-md font-bold">{order._id}</p>
+                <p
+                  className={`text-md font-bold ${order.orderStatus === "pending" ? "" : ""}`}
+                >
+                  {order.orderStatus}
                 </p>
-                <FaChevronRight className='text-orange-600'/>
-
+                <p className="text-md font-bold">{order.paymentStatus}</p>
+                <div
+                  onClick={() => {
+                    navigate(`/orders/${order._id}`);
+                    window.scrollTo({ top: 0, behavior: "smooth" });
+                  }}
+                  className="flex gap-2 items-center"
+                >
+                  <p className="cursor-pointer  font-bold hover:text-orange-600">
+                    View Detail
+                  </p>
+                  <FaChevronRight className="text-orange-600" />
                 </div>
-
-           </div>
-        ))
-      }
+              </div>
+            ))}
+        </div>
       </div>
     </div>
-      
-    </div>
-  )
-}
+  );
+};
 
-export default Order
+export default Order;
