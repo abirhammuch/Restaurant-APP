e27a65f2ed9da3a701f92034db361018e29a82b1
abirham <abirhammuch526@gmail.com>
2026-07-22 13:22:29 +0300

    jj

--- frontend/src/pages/adminpage/AdminChat.jsx
+++ frontend/src/pages/adminpage/AdminChat.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo, useState } from "react";
+import React, { useContext, useMemo, useState, useEffect } from "react";
 import { AppContext } from "../../context/AppContext";
 import { FaPaperPlane } from "react-icons/fa";
 
